24de288e2cf4d8ad0a054a6ec022f8bdc6d157b0
Om_15 <omwaghchoure15@gmail.com>
2025-07-03 00:59:41 +0530

    2/07/2025

--- src/Ps5.java
+++ src/Ps5.java
@@ -1,5 +1,13 @@
 public class Ps5 {
     public static void main(String[] args) {
-
+        Q1();
+    }
+    public static void Q1(){
+        for (int i = 5; i > 0 ;i--){
+            System.out.print("\n");
+            for (int j = 0; j < i;j++){
+                System.out.print(" * ");
+            }
+        }
     }
 }
