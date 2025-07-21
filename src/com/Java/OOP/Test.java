6efca93920a5fdf1c8bd1d4d7f0e7df2bc938baf
Om_15 <omwaghchoure15@gmail.com>
2025-07-21 16:04:47 +0530

    25/07/2025

---
+++ src/com/Java/OOP/Test.java
@@ -0,0 +1,12 @@
+package com.Java.OOP;
+
+public class Test extends Car {
+    public static void main(String[] args) {
+
+        Car myCar = new Car(); // create Object of Car Class
+        myCar.speed = 160;
+        myCar.model = " Toyota Fortuner ";
+        myCar.color = "White";
+        myCar.Drive();
+    }
+}
